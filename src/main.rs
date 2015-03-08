1f8ecdeb1557683c1bcacf7feb7a0478a7dcbbee
kennytm <kennytm@gmail.com>
2015-03-08 17:40:09 +0800

    rustup.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#![feature(path, fs, io, core, collections, std_misc)]
+#![feature(path, io, core, collections, std_misc)]
 #![feature(plugin)]
 #![plugin(regex_macros, docopt_macros)]
 
